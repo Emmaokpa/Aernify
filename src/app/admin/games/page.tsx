7a6a17ee3dfcc188937ef5543cdd1ce1defede33
phoenix marketing <phoenixmarketing21@gmail.com>
2025-12-07 15:57:53 +0000

    Ok now uploading game info is no more an issue, the next issue is the ga

--- src/app/admin/games/page.tsx
+++ src/app/admin/games/page.tsx
@@ -47,7 +47,7 @@ export default function AdminGamesPage() {
   const { toast } = useToast();
 
   const gamesCollectionRef = useMemoFirebase(
-    () => collection(firestore, 'games'),
+    () => (firestore ? collection(firestore, 'games') : null),
     [firestore]
   );
   
